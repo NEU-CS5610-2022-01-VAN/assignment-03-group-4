bb4daf546ac897dd4ff06dd65342bfce5be15f43
Yun Feng <iostream3100@gmail.com>
2022-04-05 19:49:56 -0700

    generate prisma schema model
    add create and get all users function

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-// import pkg from "@prisma/client";
+import pkg from "@prisma/client";
 import Joi from "joi";
 import cors from "cors";
 import morgan from "morgan";
@@ -12,16 +12,30 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan("dev"));
 
-// const { PrismaClient } = pkg;
-// const prisma = new PrismaClient();
+const { PrismaClient } = pkg;
+const prisma = new PrismaClient();
 
 const validator = createValidator({});
 
+app.post("/users", async (req, res) => {
+  const newUser = await prisma.user.create({
+    data: {
+      email: req.body.email,
+      name: req.body.name,
+      password: req.body.password,
+    },
+  });
+
+  res.send(newUser);
+});
+
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.send(users);
+});
+
 const port = parseInt(process.env.PORT) || 3000;
 
-app.get("/", (req, res) => {
-  res.send("<h1>Server is working.</h1>");
-});
 app.listen(port, () => {
   console.log(`App running on port: ${port}`);
 });
