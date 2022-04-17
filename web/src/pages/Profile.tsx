04efb9b08486f57dbac7188562e8b88c4978c3e1
Yun Feng <iostream3100@gmail.com>
2022-04-16 20:26:04 -0700

    Add a public profile component

--- web/src/pages/Profile.tsx
+++ web/src/pages/Profile.tsx
@@ -5,39 +5,24 @@ import { useParams } from "react-router-dom";
 import RecipeList from "../components/RecipeList";
 import ReviewList from "../components/ReviewList";
 import ProfileCard from "../components/ProfileCard";
-import LoginButton from "../components/LoginButton";
-
-const baseUrl = process.env.REACT_APP_API_BASE_URL + "/users/";
+import PublicProfile from "../components/PublicProfile";
 
 const Profile = () => {
   const params = useParams();
   const userId = params.userId;
-  const url = baseUrl + params.userId;
 
-  const {
-    isLoading,
-    error,
-    data: user,
-    isFetching,
-  } = useQuery("user", () => axios.get(url).then((res) => res.data));
+  // const {
+  //   isLoading,
+  //   error,
+  //   data: user,
+  //   isFetching,
+  // } = useQuery("user", () => axios.get(url).then((res) => res.data));
 
   return (
     <>
       {!userId && <ProfileCard />}
 
-      {userId && !isLoading && !error && (
-        <>
-          <div>
-            <h2>{user.name}'s recipes</h2>
-            <RecipeList url={url + "/recipes"} />
-          </div>
-
-          <div>
-            <h2>{user.name}'s reviews</h2>
-            <ReviewList url={url + "/reviews"} />
-          </div>
-        </>
-      )}
+      {userId && <PublicProfile userId={userId} />}
     </>
   );
 };
