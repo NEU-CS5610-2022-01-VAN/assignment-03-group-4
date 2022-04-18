95883f7fcd70c6b0e21e52dce7e9c059a8b76bd1
Yun Feng <iostream3100@gmail.com>
2022-04-17 19:45:51 -0700

    update profile page

--- web/src/pages/Profile.tsx
+++ web/src/pages/Profile.tsx
@@ -1,3 +1,5 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 
 import LoginButton from "../components/LoginButton";
@@ -5,15 +7,31 @@ import ProfileCard from "../components/ProfileCard";
 import PublicProfile from "../components/PublicProfile";
 
 const Profile = () => {
+  const { user, isAuthenticated, isLoading } = useAuth0();
+
   const params = useParams();
+  console.log(user);
+
   const userId = params.userId;
 
   return (
     <>
-      <LoginButton>Log in!!!!</LoginButton>
-      {!userId && <ProfileCard />}
+      <LoginButton>Log in (Bug to fix in the Dropdown)</LoginButton>
 
-      {userId && <PublicProfile userId={userId} />}
+      {userId ? (
+        <PublicProfile userId={userId} />
+      ) : (
+        <>
+          <ProfileCard />
+
+          {isAuthenticated &&
+            (isLoading ? (
+              <div>Loading</div>
+            ) : (
+              <PublicProfile userId={(user as any).sub} />
+            ))}
+        </>
+      )}
     </>
   );
 };
