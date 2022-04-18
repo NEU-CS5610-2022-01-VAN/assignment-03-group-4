953b1353f000ebed9492040ad5d0abb7be06d4eb
Tokaji <zimingjia7@gmail.com>
2022-04-18 01:30:07 -0700

    nav button style

--- web/src/components/LoginButton.tsx
+++ web/src/components/LoginButton.tsx
@@ -3,7 +3,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 const LoginButton = ({ children }) => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>{children}</button>;
+  return <button style={{width: "100%"}} onClick={() => loginWithRedirect()}>{children}</button>;
 };
 
 export default LoginButton;
