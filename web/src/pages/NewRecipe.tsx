1c06f4863d8157b9d192db07162794860440470c
Yun Feng <iostream3100@gmail.com>
2022-04-19 22:43:50 -0700

    can upload image in the front-end

--- web/src/pages/NewRecipe.tsx
+++ web/src/pages/NewRecipe.tsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import axios from "axios";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import { useNavigate } from "react-router-dom";
 
 import { useAuthToken } from "../components/AuthTokenContext";
+import UploadImage from "../components/UploadImage";
 
 const NewRecipe = () => {
   const { accessToken } = useAuthToken();
   const navigate = useNavigate();
+
+  const [images, setImages] = useState<any>([]);
+  const [imageUrls, setImageUrls] = useState<any>([]);
+
+  useEffect(() => {
+    if (images.length < 1) return;
+
+    const newImageUrls = images.map((image) => URL.createObjectURL(image));
+    setImageUrls(newImageUrls);
+  }, [images]);
+
+  function onImageChange(e) {
+    setImages([...e.target.files]);
+  }
 
   return (
     <>
@@ -25,18 +40,35 @@ const NewRecipe = () => {
           }
           return errors;
         }}
-        onSubmit={(values: any, { setSubmitting }) => {
-          setTimeout(() => {
-            axios
-              .post(`${process.env.REACT_APP_API_BASE_URL}/recipes/`, values, {
-                headers: { Authorization: `Bearer ${accessToken}` },
-              })
-              .then((res) => {
-                alert("Success");
-                setSubmitting(false);
-                navigate("/recipe/" + (res.data as any).id);
-              })
-              .catch((err) => console.log(err));
+        onSubmit={async (values: any, { setSubmitting }) => {
+          setTimeout(async () => {
+            try {
+              const res = await axios.post(
+                `${process.env.REACT_APP_API_BASE_URL}/recipes/`,
+                values,
+                {
+                  headers: { Authorization: `Bearer ${accessToken}` },
+                }
+              );
+              const newRecipe = res.data;
+
+              const formData = new FormData();
+              formData.append("file", images[0]);
+
+              const fileRes = await axios.post(
+                `${process.env.REACT_APP_API_BASE_URL}/recipes/${newRecipe.id}/upload`,
+                formData,
+                {
+                  headers: { Authorization: `Bearer ${accessToken}` },
+                }
+              );
+
+              alert("Success");
+              setSubmitting(false);
+              navigate("/recipe/" + newRecipe.id);
+            } catch (err) {
+              console.log(err);
+            }
           }, 200);
         }}
       >
@@ -71,6 +103,15 @@ const NewRecipe = () => {
           </Form>
         )}
       </Formik>
+
+      <>
+        <input type="file" multiple accept="image/*" onChange={onImageChange} />
+        {imageUrls.map((imageSrc) => (
+          <img style={{ width: 400 }} src={imageSrc} alt={imageSrc} />
+        ))}
+      </>
+
+      {/* <UploadImage /> */}
     </>
   );
 };
