248e8f12a51d9e423bccc218b1bbfbaf87fc8ec4
Daisy <daisybiubiubiu@163.com>
2022-04-26 01:03:43 -0700

    before merge

--- web/src/components/Popular.tsx
+++ web/src/components/Popular.tsx
@@ -2,7 +2,6 @@ import GetImageById from "../api/ImageAPI";
 import { Skeleton } from "@mui/material";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
-import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
 const Item = ({ photoId, recipeId }) => {
