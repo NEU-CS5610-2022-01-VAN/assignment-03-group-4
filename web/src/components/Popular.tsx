aa55f7cf122305da00dde4478e0eed6f55b3edf3
Daisy <daisybiubiubiu@163.com>
2022-04-26 00:56:11 -0700

    bug

--- web/src/components/Popular.tsx
+++ web/src/components/Popular.tsx
@@ -2,11 +2,17 @@ import GetImageById from "../api/ImageAPI";
 import { Skeleton } from "@mui/material";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Item = ({ photoId, recipeId }) => {
   const { isLoading, error, data: photoUrl } = GetImageById(photoId, recipeId);
+  const navigate = useNavigate();
+  const handleKeyPress = (e) => {
+    navigate(`/recipe/${recipeId}`);
+  };
   return (
-    <div>
+    <div onClick={handleKeyPress}>
       {error ? (
         <div>Error: {(error as any).mesasge}</div>
       ) : isLoading ? (
@@ -31,9 +37,15 @@ const Item = ({ photoId, recipeId }) => {
 
 const Popular = ({ recipes }) => {
   return (
-    <Carousel showStatus={false}>
+    <Carousel showStatus={false} interval={2000}>
       {recipes.map((recipe) => {
-        return <Item photoId={recipe.photos[0]} recipeId={recipe.id} />;
+        return (
+          <Item
+            key={recipe.id}
+            photoId={recipe.photos[0]}
+            recipeId={recipe.id}
+          />
+        );
       })}
     </Carousel>
   );
