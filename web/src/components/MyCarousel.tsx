ae927ef93d8e129bd5201ae35206d38ab883f79a
Daisy <daisybiubiubiu@163.com>
2022-04-30 23:22:09 -0700

    fit video

--- web/src/components/MyCarousel.tsx
+++ web/src/components/MyCarousel.tsx
@@ -1,10 +1,7 @@
-import "./css/myCarousel.css";
 import axios from "axios";
 import { useState, useEffect } from "react";
-
-type ThumbnailProps = { url: string };
-
-const Thumbnail = ({ url }: ThumbnailProps): JSX.Element => {
+import "./css/myCarousel.css";
+const Thumbnail = (props) => {
   return (
     <div
       style={{
@@ -13,57 +10,46 @@ const Thumbnail = ({ url }: ThumbnailProps): JSX.Element => {
         padding: "10px",
         height: "100px",
         backgroundPosition: "center",
-        backgroundImage: `url(${url})`,
+        backgroundImage: `url(${props.url})`,
         backgroundSize: "cover",
       }}
     />
   );
 };
 
-type Props = {
-  video: string;
-  photos: string[];
-  recipeId: string;
-  children: React.ReactNode;
-};
-
-const MyCarousel = ({
-  video,
-  photos,
-  recipeId,
-  children,
-}: Props): JSX.Element => {
+function MyCarousel(props) {
   const [urlCurrent, setUrlCurrent] = useState(
-    video
-      ? `https://www.youtube.com/embed/${video}`
+    props.video
+      ? `https://www.youtube.com/embed/${props.video}`
       : `http://img.zcool.cn/community/017f365d157e1ea8012051cd848a88.gif`
   );
-  const [videoOpen, setVideoOpen] = useState<boolean>(video ? true : false);
+  const [video, setVideo] = useState(props.video ? true : false);
+
   const [images, setImages] = useState([]);
 
   useEffect(() => {
     Promise.all(
-      photos.map((photoId) =>
+      props.photos.map((photoId) =>
         axios
           .get(
-            `${process.env.REACT_APP_API_BASE_URL}/recipes/${recipeId}/files/${photoId}`,
+            `${process.env.REACT_APP_API_BASE_URL}/recipes/${props.recipeId}/files/${photoId}`,
             { responseType: "blob" }
           )
           .then((res) => URL.createObjectURL(res.data as any))
       )
     ).then((res: any) => {
       setImages(res);
-      if (!video) {
+      if (!props.video) {
         setUrlCurrent(res[0]);
       }
     });
-  }, [photos, recipeId, video]);
+  }, [props.photos, props.recipeId, props.video]);
 
   return (
     <>
       <div className="responsive-topcard flex md:flex-row flex-col w-full top-container">
         <div className="box ">
-          {videoOpen ? (
+          {video ? (
             <iframe
               title="recipe-video"
               className="responsive-iframe"
@@ -89,7 +75,7 @@ const MyCarousel = ({
             backgroundColor: "#F5F1E7",
           }}
         >
-          {children}
+          {props.children}
         </div>
       </div>
       <div
@@ -105,37 +91,40 @@ const MyCarousel = ({
           scrollbarWidth: "none",
         }}
       >
-        {video && (
-          <button
-            onClick={() => {
-              setUrlCurrent(`https://www.youtube.com/embed/${video}`);
-              setVideoOpen(true);
-            }}
-          >
-            <Thumbnail
-              url={`https://img.youtube.com/vi/${video}/mqdefault.jpg`}
-            />
-          </button>
-        )}
-
-        {images.length !== 0 &&
-          images.map((img, index) => {
-            return (
-              <button
-                key={img}
-                onClick={() => {
-                  setUrlCurrent(img);
-                  setVideoOpen(false);
-                }}
-                aria-label={`carousel image ${index}`}
-              >
-                <Thumbnail url={img} />
-              </button>
-            );
-          })}
+        <>
+          {props.video && (
+            <button
+              onClick={() => {
+                setUrlCurrent(`https://www.youtube.com/embed/${props.video}`);
+                setVideo(true);
+              }}
+            >
+              <Thumbnail
+                url={`https://img.youtube.com/vi/${props.video}/mqdefault.jpg`}
+              />
+            </button>
+          )}
+        </>
+        <>
+          {images.length !== 0 &&
+            images.map((img, index) => {
+              return (
+                <button
+                  key={img}
+                  onClick={() => {
+                    setUrlCurrent(img);
+                    setVideo(false);
+                  }}
+                  aria-label={`carousel image ${index}`}
+                >
+                  <Thumbnail url={img} />
+                </button>
+              );
+            })}
+        </>
       </div>
     </>
   );
-};
+}
 
 export default MyCarousel;
